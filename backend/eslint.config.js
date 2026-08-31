7357828cd936b9904fd8b33e1ab4fb86c495d58a
Stephin David <stephindavid884@gmail.com>
2026-08-31 13:52:26 +0530

    Initial commit

---
+++ backend/eslint.config.js
@@ -0,0 +1,42 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+export default [
+  {
+    ignores: ["node_modules/**", "dist/**"],
+  },
+
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+        // tsconfigRootDir: import.meta.dirname
+      },
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+      },
+    },
+
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "argsIgnorePattern": "^_",
+          "varsIgnorePattern": "^_"
+        }
+      ],
+      "@typescript-eslint/explicit-function-return-type": "off",
+
+      "no-console": "error",
+      "no-process-env": "off",
+    },
+  },
+];
---
+++ backend/src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log('Server started...')
+});
